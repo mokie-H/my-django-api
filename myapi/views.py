b8ca808488f55dbf72d48550779cecd2f4e82e43
mekonnen <mekonnenhaileslassie@gmail.com>
2020-09-30 23:57:19 +0300

    removing the render shortcut from view.py b/c is api and has not good place to it

--- myapi/views.py
+++ myapi/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
